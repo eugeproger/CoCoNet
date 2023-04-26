fcea7fe9a25f577d62e20f059ce4635f470a406a
Euge <euge.itech@gmail.com>
2023-04-26 09:40:43 +0200

    pouse

--- app/src/main/java/com/eugeproger/coconet/tabs/contact/ContactsFragment.java
+++ app/src/main/java/com/eugeproger/coconet/tabs/contact/ContactsFragment.java
@@ -2,65 +2,125 @@ package com.eugeproger.coconet.tabs.contact;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.eugeproger.coconet.R;
+import com.eugeproger.coconet.simple.Contact;
+import com.eugeproger.coconet.support.Constant;
+import com.eugeproger.coconet.support.FirebaseConfiguration;
+import com.eugeproger.coconet.support.FirebaseFolderName;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ContactsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import de.hdodenhof.circleimageview.CircleImageView;
+
+
 public class ContactsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private View contactsView;
+    private RecyclerView mContactsList;
+    private DatabaseReference contactsRef, usersRef;
+    private FirebaseAuth auth;
+    private String currentUserID;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ContactsFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ContactsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ContactsFragment newInstance(String param1, String param2) {
-        ContactsFragment fragment = new ContactsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contacts, container, false);
+        contactsView = inflater.inflate(R.layout.fragment_contacts, container, false);
+        mContactsList = (RecyclerView) contactsView.findViewById(R.id.contacts_list_frag_contacts);
+        mContactsList.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        contactsRef = FirebaseConfiguration.setRealtimeDatabaseConfiguration().child(FirebaseFolderName.CONTACTS).child(currentUserID);
+        usersRef = FirebaseConfiguration.setRealtimeDatabaseConfiguration();
+        currentUserID = auth.getCurrentUser().getUid();
+
+        return contactsView;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerOptions options = new FirebaseRecyclerOptions.Builder<Contact>().setQuery(contactsRef, Contact.class).build();
+
+        final FirebaseRecyclerAdapter<Contact, ContactsViewHolder> adapter = new FirebaseRecyclerAdapter<Contact, ContactsViewHolder>(options) {
+            @NonNull
+            @Override
+            public ContactsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.user_box_layout, parent,false);
+                ContactsViewHolder viewHolder = new ContactsViewHolder(view);
+                return viewHolder;
+            }
+
+            @Override
+            protected void onBindViewHolder(@NonNull ContactsViewHolder holder, int position, @NonNull Contact model) {
+                String userIDs = getRef(position).getKey();
+                usersRef.child(userIDs).addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if (snapshot.hasChild(Constant.IMAGE)) {
+                            String profileImage = snapshot.child(Constant.IMAGE).getValue().toString();
+                            String profileBio = snapshot.child(Constant.BIO).getValue().toString();
+                            String profileName = snapshot.child(Constant.NAME).getValue().toString();
+
+                            holder.userName.setText(profileName);
+                            holder.userBio.setText(profileBio);
+                            Picasso.get().load(profileImage).placeholder(R.drawable.profile_image).into(holder.userImage);
+                        }
+                        else {
+                            String profileBio = snapshot.child(Constant.BIO).getValue().toString();
+                            String profileName = snapshot.child(Constant.NAME).getValue().toString();
+
+                            holder.userName.setText(profileName);
+                            holder.userBio.setText(profileBio);
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+            }
+        };
+
+        mContactsList.setAdapter(adapter);
+        adapter.startListening();
+    }
+
+    public static class ContactsViewHolder extends RecyclerView.ViewHolder {
+
+        TextView userName, userBio;
+        CircleImageView userImage;
+
+        public ContactsViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            userName = itemView.findViewById(R.id.name_user_box_layout);
+            userBio = itemView.findViewById(R.id.bio_user_box_layout);
+            userImage = itemView.findViewById(R.id.profile_image_user_box_layout);
+
+        }
     }
 }
